b0e134f9df0e7b78e03cba2a02863533963b9239
sakirZ <sxvflove@gmail.com>
2024-08-21 01:23:42 +0900

    1시23분

--- Assets/Scripts/Ui_interact.cs
+++ Assets/Scripts/Ui_interact.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Ui_interact : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+
+
+
+
+
+    public void Scene_change()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
 }
